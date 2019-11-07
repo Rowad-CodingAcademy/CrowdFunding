cc0a8b899429eafdb0d7973d52d8207eadd73b20
AbdulrahmanShaiban <Aboodshi777@gmail.com>
2019-11-07 16:31:02 +0300

    Fix Firebase Errors

--- app/src/main/java/com/abood/crowdfunding/LoginActivity.java
+++ app/src/main/java/com/abood/crowdfunding/LoginActivity.java
@@ -2,20 +2,19 @@ package com.abood.crowdfunding;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
     Button loginBTN,signupBTN;
     EditText userEmail,userPass;
     FirebaseAuth auth;
@@ -41,8 +40,12 @@ public class LoginActivity extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
 
                                 if(task.isSuccessful()){
-                                    Intent afterIntnet=new Intent(LoginActivity.this,LoginActivity.class);
-                                    startActivity(afterIntnet);
+
+//                                    Intent afterIntnet=new Intent(LoginActivity.this,LoginActivity.class);
+//                                    startActivity(afterIntnet);
+
+                                    Toast.makeText(LoginActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+
                                 }
                                 else {
                                     Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/abood/crowdfunding/RegisterActivity.java
+++ app/src/main/java/com/abood/crowdfunding/RegisterActivity.java
@@ -2,19 +2,18 @@ package com.abood.crowdfunding;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
     EditText newUserPass,newUserEmail;
     Button signupBTN;
     FirebaseAuth auth;
@@ -27,6 +26,9 @@ public class RegisterActivity extends AppCompatActivity {
         newUserPass=findViewById(R.id.new_user_pass);
         newUserEmail=findViewById(R.id.new_user_email);
         signupBTN=findViewById(R.id.signup_btn);
+
+        auth=FirebaseAuth.getInstance();
+
         signupBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,8 +39,6 @@ public class RegisterActivity extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if(task.isSuccessful()){
                                     Toast.makeText(RegisterActivity.this, "User Created Successfully", Toast.LENGTH_SHORT).show();
-
-
 
                                 }
                                 else {
