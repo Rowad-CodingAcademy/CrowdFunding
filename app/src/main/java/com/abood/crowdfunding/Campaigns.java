a6a472694c27ef5bfbd395d73fc01113790eb491
Lujain <lujainsweet12@gmail.com>
2019-11-13 15:33:53 +0300

    Fix model Error

--- app/src/main/java/com/abood/crowdfunding/Campaigns.java
+++ app/src/main/java/com/abood/crowdfunding/Campaigns.java
@@ -1,22 +1,18 @@
 package com.abood.crowdfunding;
 
 import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.Date;
 
 public class Campaigns {
 
-    private String campTitle;
-    private String campDescription;
-    private Date campStart;
-    private Date campEnd;
-    private String campTarget;
-    private String campLocation;
-    private String campCountry;
-    private String campImageUrl;
-    private String campVideoUrl;
-
+    private String pId;
+    private String pUser;
+    private Date Date;
+    private String pDate;
+    private String pQuestion;
+    private String pType;
+    private Boolean pAction;
+    private String pImage;
 
     public Campaigns() {
 
